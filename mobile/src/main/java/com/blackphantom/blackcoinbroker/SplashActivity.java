0b9695fec946a7fdc8d661bd0fb046c8daac2b5d
Spookyfication <k.abstein@hotmail.de>
2017-05-29 19:09:17 +0200

    added SplashScreen

---
+++ mobile/src/main/java/com/blackphantom/blackcoinbroker/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.blackphantom.blackcoinbroker;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by BPS on 29.05.2017.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
